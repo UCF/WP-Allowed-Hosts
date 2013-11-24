156e067b1eda71716bd10bfbf6f58e62cd3d1d4d
Brandon Groves <brandon.groves@gmail.com>
2013-11-23 19:08:57 -0500

    using checked method provided by WordPress

--- options.php
+++ options.php
@@ -8,7 +8,7 @@
                 <td>
                     <textarea name="allowed-hosts"><?php echo get_option('allowed-hosts'); ?></textarea>
                     <br />
-                    <input type="checkbox" name="allowed-hosts-regex" id="allowed-hosts-regex" value="<?php echo get_option('allowed-hosts-regex'); ?>" /> <label for="allowed-hosts-regex">Compare hosts using regular expressions</label>
+                    <input type="checkbox" name="allowed-hosts-regex" id="allowed-hosts-regex" value="1" <?php checked(get_option('allowed-hosts-regex')); ?> /> <label for="allowed-hosts-regex">Compare hosts using regular expressions</label>
                     <p>Enter domain names that this WordPress instance needs to communicate with. Separate multiple domains by commas. For information about regular expressions please go to <a href="http://www.regular-expressions.info">http://www.regular-expressions.info</a>.</p>
                 </td>
             </tr>
