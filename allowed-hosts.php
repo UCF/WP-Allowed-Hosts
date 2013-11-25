c6e9a601abb5aad5d2e22c664cecb6a68c84d361
Brandon Groves <brandon.groves@gmail.com>
2013-11-25 17:40:55 -0500

    Cleanup Oops

--- allowed-hosts.php
+++ allowed-hosts.php
@@ -143,7 +143,7 @@ class AHSettings {
                         <th scope='row'>Hosts</th>
                         <td>
                             <textarea name='allowed-hosts'>", ((is_multisite() && is_plugin_active_for_network(plugin_basename(__FILE__))) ? get_site_option('allowed-hosts') : get_option('allowed-hosts')), "</textarea>
-                            br />
+                            <br />
                             <input type='checkbox' name='allowed-hosts-regex' id='allowed-hosts-regex' value='1'", (is_multisite() && is_plugin_active_for_network(plugin_basename(__FILE__))) ? checked(get_site_option('allowed-hosts-regex')) : checked(get_option('allowed-hosts-regex')), "/> <label for='allowed-hosts-regex'>Compare hosts using regular expressions</label>
                             <p>Enter domain names that this WordPress instance needs to communicate with. Separate multiple domains by commas. Delimiters are not needed for regular expressions since they are put in for you ('/'). For information about regular expressions please go to <a href='http://www.regular-expressions.info'>http://www.regular-expressions.info</a>.</p>
                         </td>
