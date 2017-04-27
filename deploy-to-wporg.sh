2aff27ad5310237abaa73737cf4bfcc6771698ad
Derek J. Bernard <derekjbernard@gmail.com>
2017-04-27 10:43:34 -0400

    deploy-to-wporg.sh variable cleanup and cleaner output

--- deploy-to-wporg.sh
+++ deploy-to-wporg.sh
@@ -15,12 +15,12 @@ CURRENTDIR=`pwd`
 MAINFILE="allowed-hosts.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
-GITPATH="$CURRENTDIR" # this file should be in the base of your git repository
+GITPATH="${CURRENTDIR}" # this file should be in the base of your git repository
 
 # svn config
 TMP_PATH=`mktemp -d`
-SVNPATH="$TMP_PATH/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-#SVNURL="https://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNPATH="${TMP_PATH}/${PLUGINSLUG}" # path to a temp SVN repo. No trailing slash required and don't add trunk.
+#SVNURL="https://plugins.svn.wordpress.org/${PLUGINSLUG}/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNURL="https://svn.code.sf.net/p/testing-svn-sync-with-git/code/"
 COMMITMSG="Deploy to WordPress.org via Jenkins"
 
@@ -34,57 +34,57 @@ echo ".........................................."
 echo
 
 # Check version in readme.txt is the same as plugin file
-NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
-echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "Version" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}'`
-echo "$MAINFILE version: $NEWVERSION2"
+NEWVERSION1=`grep "^Stable tag" ${GITPATH}/readme.txt | awk -F' ' '{print $NF}'`
+echo "readme version: ${NEWVERSION1}"
+NEWVERSION2=`grep "Version" ${GITPATH}/${MAINFILE} | awk -F' ' '{print $NF}'`
+echo "${MAINFILE} version: ${NEWVERSION2}"
 
-if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
+if [ "${NEWVERSION1}" != "${NEWVERSION2}" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
 
 echo "Versions match in readme.txt and PHP file. Let's proceed..."
 COMMITMSG="Deploy ${PLUGINSLUG} ${NEWVERSION1} to WordPress.org via Jenkins"
 echo 
 echo "Creating local copy of SVN repo ..."
-svn co $SVNURL $SVNPATH
+svn co ${SVNURL} ${SVNPATH}
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
-git checkout-index -a -f --prefix=$SVNPATH/trunk/
+git checkout-index -a -f --prefix=${SVNPATH}/trunk/
 
 echo "Ignoring github specific & deployment script"
 svn propset svn:ignore "deploy-to-wporg.sh
 .git
-.gitignore" "$SVNPATH/trunk/"
+.gitignore" "${SVNPATH}/trunk/"
 
-if [ ! -d "$SVNPATH/assets/" ]; then
+if [ ! -d "${SVNPATH}/assets/" ]; then
 	echo "Moving assets-wp-repo"
-	mkdir $SVNPATH/assets/
-	mv $SVNPATH/trunk/assets-wp-repo/* $SVNPATH/assets/
-	svn add $SVNPATH/assets/
-	svn delete $SVNPATH/trunk/assets-wp-repo
+	mkdir ${SVNPATH}/assets/
+	mv ${SVNPATH}/trunk/assets-wp-repo/* ${SVNPATH}/assets/
+	svn add ${SVNPATH}/assets/
+	svn delete ${SVNPATH}/trunk/assets-wp-repo
 fi
 
 echo "Changing directory to SVN"
-cd $SVNPATH/trunk/
+cd ${SVNPATH}/trunk/
 # Add all new files that are not set to be ignored
 echo "committing to trunk"
 set +x
-echo 'svn commit --username=$WP_ORG_USER --password=$WP_ORG_PASS -m "$COMMITMSG"'
-echo `svn commit --username=$WP_ORG_USER --password=$WP_ORG_PASS -m "$COMMITMSG"`
+echo 'svn commit --username=${WP_ORG_USER} --password=${WP_ORG_PASS} -m '\"$COMMITMSG\"
+printf `svn commit --username=${WP_ORG_USER} --password=${WP_ORG_PASS} -m "${COMMITMSG}"`
 set -x
 echo "Check if tagged version exists"
-cd $SVNPATH
-if [ ! -d "$SVNPATH/tags/$NEWVERSION1/" ]; then
+cd ${SVNPATH}
+if [ ! -d "${SVNPATH}/tags/${NEWVERSION1}/" ]; then
 	echo "Creating new SVN tag & committing it"
-	svn copy trunk/ tags/$NEWVERSION1/
-	cd $SVNPATH/tags/$NEWVERSION1
+	svn copy trunk/ tags/${NEWVERSION1}/
+	cd ${SVNPATH}/tags/${NEWVERSION1}
 	set +x
-	echo 'svn commit --username=$WP_ORG_USER --password=$WP_ORG_PASS -m "Tagging version $NEWVERSION1"'
-	echo `svn commit --username=$WP_ORG_USER --password=$WP_ORG_PASS -m "Tagging version $NEWVERSION1"`
+	echo 'svn commit --username=${WP_ORG_USER} --password=${WP_ORG_PASS} -m '\"Tagging version ${NEWVERSION1}\"
+	printf `svn commit --username=${WP_ORG_USER} --password=${WP_ORG_PASS} -m "Tagging version ${NEWVERSION1}"`
 	set -x
 fi
 
-cd $GITPATH
-echo "Removing temporary directory $SVNPATH"
-rm -fr $TMP_PATH/
+cd ${GITPATH}
+echo "Removing temporary directory ${SVNPATH}"
+rm -fr ${TMP_PATH}/
 
 echo "*** FIN ***"
